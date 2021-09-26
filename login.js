be9bfe32bb4da612904770dcfd843c2ae60fd8b6
ishita-lyall <ishitalyall23@gmail.com>
2021-09-26 18:47:47 +0530

    nav redone

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 // import showPosition from '../parking/listspot.html'
+require("firebase/firestore");
 var firebaseConfig = {
     apiKey: "AIzaSyCqEHCURjJNlcKNgNms_mVrTp6uNcDO41E",
     authDomain: "parking-shybois.firebaseapp.com",
@@ -9,6 +10,7 @@ var firebaseConfig = {
     measurementId: "G-7MLR29NEE1"
 };
 // Initialize Firebase
+const firebase = require("firebase");
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
@@ -79,13 +81,22 @@ function showPosition(position) {
   }
 
 async function uploadspot() {
+    await firebase.initializeApp(firebaseConfig);
     var area = document.getElementById("area_id")
+    var a=(area=="commercial")?"C":"R"
     var spots = document.getElementById("spots_id")
+    var vehicle = document.getElementById("vehicle_id")
+    var address = document.getElementById("address_id")
+    var point = new firebase.firestore.GeoPoint(position.latitude,position.longitude)
     showPosition();
-    db.collection("parking").add({
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
+    await db.collection("parking").add({
+        area:a,
+        geo:point,
+        loc:address,
+        space: spots,
+        time:"9-22",
+        user: auth.currentUser.uid
     })
+    alert("added")
 }
 
